1058556c2ccc0d8df9b8ad73e32120c2b0581781
kronkt <125762102+kronkt@users.noreply.github.com>
2023-03-10 12:59:11 -0700

    Add files via upload
    getting back into it

---
+++ age_challenge.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+
+echo "Please enter your age: "
+read age
+
+days=$((age * 365))
+
+echo "You have lived for $days days."
---
+++ script.zsh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+
+var=1
+
+if [ var=1 ]; then
+	echo "Input: "
+	echo " "
+	read input
+	echo " "
+	echo "Input: $input"
+fi
+
+
+
+
+
+
+
